3910de8994cc19cf8e041599728ad818561e8cea
Ninjaah <ninja.luotonen@gmail.com>
2020-02-07 12:37:34 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at localhost:%s:%s", host, port)
+})
+
+module.exports = app;
